65a3cc00cd1728fa52c9d5f00d9d8e1da8c32d0f
Harry-Kwesi <harrydegraft@hotmail.com>
2024-01-18 11:23:14 +0530

    Initial Commit

---
+++ Auth-SwiftUI-Firebase/Auth_SwiftUI_FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Auth_SwiftUI_FirebaseApp.swift
+//  Auth-SwiftUI-Firebase
+//
+//  Created by Harry Kwesi De Graft on 18/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Auth_SwiftUI_FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
